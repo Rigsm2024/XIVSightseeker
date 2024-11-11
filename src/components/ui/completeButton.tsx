56f202d6a37fd08ad9dd1efe1730a0d947f279de
Rigsm2024 <mnori.fpicker@gmail.com>
2024-11-11 20:09:12 +0900

    add completed animation

--- src/components/ui/completeButton.tsx
+++ src/components/ui/completeButton.tsx
@@ -1,5 +1,6 @@
 import { GuidedSightseeingLog } from "@/features/interface/dataClass";
 import { LogFilterProps } from "@/features/shared/logSorter";
+import React, { useRef } from 'react';
 
 interface completeProps {
     glog: GuidedSightseeingLog,
@@ -10,10 +11,15 @@ interface completeProps {
 const CompleteButton = ({glog, filters, updateFilters}: completeProps) => {
     const itemNo = glog.Data.ItemNo;
     const completes = filters.completed?.slice();
+    const buttonRef = useRef<HTMLDivElement | null>(null);
 
     const makeItComplete = () => {
-        completes?.push(itemNo);
-        updateFilters({ completed: completes });
+        buttonRef.current?.classList.add('completed-animation');
+
+        setTimeout(() => {
+            completes?.push(itemNo);
+            updateFilters({ completed: completes });
+        }, 500);
     };
     const backItUncompleted = () => {
         const newCompletes = completes?.filter(f => f != itemNo);
@@ -24,12 +30,9 @@ const CompleteButton = ({glog, filters, updateFilters}: completeProps) => {
     return (
         <div onClick={onClick} className='relative mr-2 w-6 h-6 cursor-pointer active:opacity-80'>
             <div className='metalic-border z-10 w-6 h-6 rounded-full'></div>
-            <div className={`absolute right-0 z-10 w-5 h-5 top-0.5 left-0.5 rounded-full ${glog.IsCompleted ? 'bg-green-500' : 'bg-cardBase'}`}>
-                {glog.IsCompleted && (
-                    <div className='absolute top-0.5 left-1.5 border-r-2 border-b-2 border-white w-2 h-3 rotate-45'></div>
-                )}
+            <div className={`absolute right-0 z-10 w-5 h-5 top-0.5 left-0.5 rounded-full ${glog.IsCompleted ? 'bg-green-500' : 'bg-cardBase'}`}  ref={buttonRef}>
+                <div className={`absolute top-0.5 left-1.5 border-r-2 border-b-2 border-white w-2 h-3 rotate-45 ${glog.IsCompleted ? '' : 'hidden'}`} ></div>
             </div>
-            
         </div>
     );
 };
--- src/pages/_HelloWorld.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function HelloWorld() {
-    return (
-        <div>
-            <h1>Hellow World!</h1>
-            <div className='text-red-400'>How are you?</div>
-        </div>
-    )
-}
